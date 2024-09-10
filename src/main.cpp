a69daefe226967b62147faecbdb1e7a89c60ef99
Sceptive <Sceptive@gmail.com>
2024-09-09 20:02:27 -0500

    Fixed rotations and added export

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+const float PI = 3.141592653589;
+const float TAU = 2 * PI;
+
+float fmod(float a, float b) {
+    return a - (int)(a / b) * b;
+}
 
 class Vector3 {
     public:
-        float x = 0;
-        float y = 0;
-        float z = 0;
+        float x;
+        float y;
+        float z;
 
-        Vector3() {};
+        Vector3() : x(0), y(0), z(0) {};
         Vector3(float x, float y, float z) : x(x), y(y), z(z) {};
 
         friend ostream& operator<<(ostream& os, const Vector3& v) {
@@ -31,6 +40,35 @@ class Vector3 {
         Vector3 cross(const Vector3 v) {
             return Vector3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
         }
+
+        Vector3 mod(float n) {
+            return Vector3(fmod(x, n), fmod(y, n), fmod(z, n));
+        }
+};
+
+class Mat3x3 {
+    public:
+        Vector3 vx;
+        Vector3 vy;
+        Vector3 vz;
+
+        Mat3x3() : vx(Vector3(1, 0, 0)), vy(Vector3(0, 1, 0)), vz(Vector3(0, 0, 1)) {};
+        Mat3x3(Vector3 vx, Vector3 vy, Vector3 vz) : vx(vx), vy(vy), vz(vz) {};
+
+        Mat3x3& operator+=(const Mat3x3& m) {
+            this->vx += m.vx;
+            this->vy += m.vy;
+            this->vz += m.vz;
+            return *this;
+        }
+
+        Mat3x3 operator*(const float n) const {
+            return Mat3x3(vx * n, vy * n, vz * n);
+        }
+
+        Mat3x3 cross(const Vector3 v) {
+            return Mat3x3(vx.cross(v), vy.cross(v), vz.cross(v));
+        }
 };
 
 class Drone {
@@ -45,24 +83,24 @@ class Drone {
         Vector3 omega; // rad/s
 
         Vector3 position; // m
-        Vector3 theta; // rad
+        Mat3x3 theta; // rotation matrix
 
-        Drone(float m, float i) : invMass(1 / m), invInertia(1 / i) {};
+        Drone(float mass, float inertia) : invMass(1 / mass), invInertia(1 / inertia) {};
 
         void applyForce(Vector3 force, Vector3 radius) { // apply force (N) at radius (m)
-            Vector3 torque = force.cross(radius);
+            Vector3 torque = radius.cross(force);
 
-            alpha += torque * invInertia;
+            alpha = (alpha + torque * invInertia).mod(TAU);
             acceleration += force * invMass;
         }
 
-        // TODO: Heun's method?
+        // TODO: Better integrator?
         void physicsStep(float t) { // run a physics step with a fidelity of t (s)
             velocity += acceleration * t;
-            omega += alpha * t;
+            omega = (omega + alpha * t).mod(TAU);
 
             position += velocity * t;
-            theta += omega * t;
+            theta += theta.cross(omega) * t;
 
             acceleration = Vector3();
             alpha = Vector3();
@@ -72,10 +110,26 @@ class Drone {
 int main() {
     Drone d(1, 1);
 
-    d.applyForce(Vector3(0, 1, 0), Vector3(1, 0, 0));
-    
-    d.physicsStep(1);
+    d.applyForce(Vector3(0, 1, 0), Vector3(1, 0, 1));
 
-    std::cout << "Position: " << d.position << "\nRotational Position: " << d.theta << "\nLinear Velocity: " << d.velocity << "\nRotational Velocity: " << d.omega << "\nLinear Acceleration: " << d.acceleration << "\nRotational Acceleration: " << d.alpha;
+    string out = "return \"";
+    
+    for (int i = 0; i < 1000; i++) {
+        d.physicsStep(.1);
+        out += std::to_string(d.position.x) + "," + std::to_string(d.position.y) + "," + std::to_string(d.position.z) + "," + std::to_string(d.theta.vx.x) + "," + std::to_string(d.theta.vx.y) + "," + std::to_string(d.theta.vx.z) + "," + std::to_string(d.theta.vy.x) + "," + std::to_string(d.theta.vy.y) + "," + std::to_string(d.theta.vy.z) + "," + std::to_string(d.theta.vz.x) + "," + std::to_string(d.theta.vz.y) + "," + std::to_string(d.theta.vz.z);
+
+        if (i < 999) {
+            out += "|";
+        } else {
+            out += "\"";
+        }
+    }
+
+    ofstream MyFile("output.txt");
+
+    MyFile << out;
+
+    MyFile.close();
+
     return 0;
 }
